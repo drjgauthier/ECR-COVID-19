e8816c3d419543ef6377b59f883fc3f9b797e082
Jordan Gauthier <jgauthier@fredhutch.org>
2020-03-22 22:02:25 -0700

    Clean up

--- Analysis.R
+++ Analysis.R
@@ -1,7 +1,7 @@
 library(rms)
-
-fit<- glm(death~rcs(age,7),
+fit<- glm(death~rcs(age,4),
           family="binomial",data=df)
+summary(fit)
 AIC(fit)
 s<-data.frame(predict(fit,se.fit=TRUE))
 df<-bind_cols(df,s)
@@ -16,8 +16,8 @@ ggplot(df,aes(x=age)) +
   geom_line(aes(y=prob))+
   geom_ribbon(aes(ymin=prob_lower,ymax=prob_upper),alpha=0.1)+
   labs(x="Age",y="Probability of hospitalization")+
-  scale_y_continuous(breaks=c(0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80),
-                     labels=c("0.10","0.20","0.30","0.40","0.50","0.60","0.70","0.80"))+
+  # scale_y_continuous(breaks=c(0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80),
+  #                    labels=c("0.10","0.20","0.30","0.40","0.50","0.60","0.70","0.80"))+
   theme(legend.title = element_blank(),
         legend.text = element_text(size=15),
         axis.title.x = element_text(size=15),
@@ -25,3 +25,4 @@ ggplot(df,aes(x=age)) +
         axis.title.y = element_text(size=15),
         axis.text.y=element_text(size=12))+
   theme_bw()
+
