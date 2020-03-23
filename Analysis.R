71b2c1ca1b77d09500f53eed79035e4505c8cd1f
Jordan Gauthier <jgauthier@fredhutch.org>
2020-03-22 22:08:40 -0700

    Minor edits

--- Analysis.R
+++ Analysis.R
@@ -5,7 +5,6 @@ summary(fit)
 AIC(fit)
 s<-data.frame(predict(fit,se.fit=TRUE))
 df<-bind_cols(df,s)
-
 df$logit_upper <- df$fit + 1.96*df$se.fit
 df$logit_lower <- df$fit - 1.96*df$se.fit
 df$prob <- exp(df$fit)/(1 + exp(df$fit))
@@ -16,8 +15,6 @@ ggplot(df,aes(x=age)) +
   geom_line(aes(y=prob))+
   geom_ribbon(aes(ymin=prob_lower,ymax=prob_upper),alpha=0.1)+
   labs(x="Age",y="Probability of hospitalization")+
-  # scale_y_continuous(breaks=c(0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.80),
-  #                    labels=c("0.10","0.20","0.30","0.40","0.50","0.60","0.70","0.80"))+
   theme(legend.title = element_blank(),
         legend.text = element_text(size=15),
         axis.title.x = element_text(size=15),
